95078d1d4128968b8885fdd7764ed942311539bd
Mouni Bmk <mounika10.bommakanti@gmail.com>
2018-05-21 02:20:57 -0500

    Update LibraryController.java

--- src/main/java/com/grabnotes/lib/service/LibraryController.java
+++ src/main/java/com/grabnotes/lib/service/LibraryController.java
@@ -1,10 +1,7 @@
 package com.grabnotes.lib.service;
-
 import java.sql.SQLException;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.grabnotes.lib.biz.Book;
 import com.grabnotes.lib.dao.BookDAO;
 
